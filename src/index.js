3c6c19e762e6302675b8ece4b19787b7e3bd1b70
Rama Sapto <ramasapto3@gmail.com>
2022-09-29 22:31:17 +0700

    initial admin dashboard

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+// third-party
+import { Provider as ReduxProvider } from 'react-redux';
+
+// apex-chart
+import 'assets/third-party/apex-chart.css';
+
+// project import
 import App from './App';
+import { store } from 'store';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// ==============================|| MAIN - REACT DOM RENDER  ||============================== //
+
+ReactDOM.render(
+    <StrictMode>
+        <ReduxProvider store={store}>
+            <BrowserRouter basename="/free">
+                <App />
+            </BrowserRouter>
+        </ReduxProvider>
+    </StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
